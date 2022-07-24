f31de50c95601df3a3e3ea1e2f6ee35bde665e10
codeofli <MarsLee5@foxmail.com>
2022-07-24 22:44:49 +0800

    0724

---
+++ src/main/java/codeofli/leetcode/contest/c303/FoodRatings6126.java
@@ -0,0 +1,90 @@
+package codeofli.leetcode.contest.c303;
+
+import codeofli.my.leetcode.StringTransformUtil;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class FoodRatings6126 {
+    /**
+     * 1 <= n <= 2 * 104
+     */
+    static class FoodRatings {
+        /**
+         * 有序+动态查找 平衡树-红黑树
+         */
+        HashMap<String, TreeMap<Integer, ArrayList<String>>> cuisinesMap;
+        HashMap<String, Integer> rateMap;
+        HashMap<String, String> cuiMap;
+
+        public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+            // <cuiName,treeMap>
+            cuisinesMap = new HashMap<>();
+            //<food,rate>
+            rateMap = new HashMap<>();
+            //<food,cui>
+            cuiMap = new HashMap<>();
+            int n = foods.length;
+            for (int i = 0; i < n; i++) {
+                TreeMap<Integer, ArrayList<String>> treeMap = cuisinesMap.getOrDefault(cuisines[i], new TreeMap<Integer, ArrayList<String>>());
+                ArrayList<String> list = treeMap.getOrDefault(ratings[i], new ArrayList<String>());
+                list.add(foods[i]);
+                treeMap.put(ratings[i], list);
+                rateMap.put(foods[i], ratings[i]);
+                cuiMap.put(foods[i], cuisines[i]);
+                cuisinesMap.put(cuisines[i], treeMap);
+            }
+        }
+
+        public void changeRating(String food, int newRating) {
+            Integer rate = rateMap.get(food);
+            //修改分数映射
+            rateMap.put(food,newRating);
+            String cui = cuiMap.get(food);
+            TreeMap<Integer, ArrayList<String>> treeMap = cuisinesMap.get(cui);
+            ArrayList<String> list = treeMap.get(rate);
+            list.remove(food);
+            if (list.size() == 0) {
+                treeMap.remove(rate);
+            } else {
+                treeMap.put(rate, list);
+            }
+            ArrayList<String> list1 = treeMap.getOrDefault(rate, new ArrayList<String>());
+            list1.add(food);
+            treeMap.put(newRating, list1);
+        }
+
+        public String highestRated(String cuisine) {
+            TreeMap<Integer, ArrayList<String>> treeMap = cuisinesMap.get(cuisine);
+            if (treeMap == null) {
+                return null;
+            }
+            ArrayList<String> list = treeMap.lastEntry().getValue();
+            list.sort(String::compareTo);
+            return list.get(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        FoodRatings foodRatings = new FoodRatings(StringTransformUtil.toStringArray("[\"kimchi\",\"miso\",\"sushi\",\"moussaka\",\"ramen\",\"bulgogi\"]"),
+                StringTransformUtil.toStringArray("[\"korean\",\"japanese\",\"japanese\",\"greek\",\"japanese\",\"korean\"]"
+                ), StringTransformUtil.toIntArray("[9,12,8,15,14,7]"));
+        String s = "[[\"korean\"],[\"japanese\"],[\"sushi\",16],[\"japanese\"],[\"ramen\",16],[\"japanese\"]]";
+        String[] strings = StringTransformUtil.toStringArray(s);
+        String opt = "[\"highestRated\",\"highestRated\",\"changeRating\",\"highestRated\",\"changeRating\",\"highestRated\"]";
+        String[] opts = StringTransformUtil.toStringArray(opt);
+        //int i = 0;
+        for (int i = 0, j = 0; i < opts.length; i++) {
+            if ("highestRated".equals(opts[i])) {
+                System.out.println(foodRatings.highestRated(strings[j]));
+                j++;
+            } else {
+                //String[] split = StringTransformUtil.toStringArray(strings[i]);
+                foodRatings.changeRating(strings[j++], Integer.parseInt(strings[j++]));
+                System.out.println("null");
+            }
+        }
+    }
+}
