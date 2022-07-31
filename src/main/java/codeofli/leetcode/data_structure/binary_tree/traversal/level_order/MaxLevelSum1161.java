a691acb184c22c6a21ba15c6c094b80062d410a8
codeofli <MarsLee5@foxmail.com>
2022-07-31 22:52:51 +0800

    2022-7-31

---
+++ src/main/java/codeofli/leetcode/data_structure/binary_tree/traversal/level_order/MaxLevelSum1161.java
@@ -0,0 +1,40 @@
+package codeofli.leetcode.data_structure.binary_tree.traversal.level_order;
+
+import codeofli.leetcode.data_structure.binary_tree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaxLevelSum1161 {
+    /**
+     * 层次遍历
+     */
+    public int maxLevelSum(TreeNode root) {
+        //树中的节点数在 [1, 104]范围内
+        int resLevel = -1;
+        int maxSum = Integer.MIN_VALUE;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int levelCnt = 1;
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            int sum = 0;
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if(node.left != null){
+                    queue.add(node.left);
+                }
+                if(node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            if(sum > maxSum){
+                maxSum = sum;
+                resLevel =levelCnt;
+            }
+            levelCnt++;
+        }
+        return resLevel;
+    }
+}
