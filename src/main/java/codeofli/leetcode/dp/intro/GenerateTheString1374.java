285ced3758b62829fcbea231a06e4042555a608d
codeofli <MarsLee5@foxmail.com>
2022-08-02 23:27:36 +0800

    2022-8-2

---
+++ src/main/java/codeofli/leetcode/dp/intro/GenerateTheString1374.java
@@ -0,0 +1,30 @@
+package codeofli.leetcode.dp.intro;
+
+import java.util.Arrays;
+
+public class GenerateTheString1374 {
+    public String generateTheString(int n) {
+        StringBuffer sb = new StringBuffer();
+        if (n % 2 == 1) {
+            return sb.append("a".repeat(n)).toString();
+        }
+        return sb.append("a".repeat(n - 1)).append("b").toString();
+    }
+
+
+    public String generateTheString2(int n) {
+        if(n % 2 == 1){
+            return  getNCharString('a',n);
+        }else{
+            return getNCharString('a',n-1)+'b';
+        }
+    }
+
+    String getNCharString(char a,int n){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i <n; i++){
+            sb.append(a);
+        }
+        return sb.toString();
+    }
+}
