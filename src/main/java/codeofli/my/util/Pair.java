0bb81ad10bf89a2a730dbbea9bb8f019c325a0ff
mingzli <nicolaslee5@foxmail.com>
2022-09-05 23:41:28 +0800

    2022-9-5

---
+++ src/main/java/codeofli/my/util/Pair.java
@@ -0,0 +1,14 @@
+package codeofli.my.util;
+
+/**
+ * 通用泛型Pair类型
+ */
+public class Pair<V1, V2> {
+    public V1 val1;
+    public V2 val2;
+
+    public Pair(V1 val1, V2 val2) {
+        this.val1 = val1;
+        this.val2 = val2;
+    }
+}
