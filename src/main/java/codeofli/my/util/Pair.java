0937c8e2fd1d3ed26a998fc0ee3e1007f716c7ca
mingzli <nicolaslee5@foxmail.com>
2022-09-12 20:40:02 +0800

    2022-9-12

--- src/main/java/codeofli/my/util/Pair.java
+++ src/main/java/codeofli/my/util/Pair.java
@@ -6,6 +6,7 @@ package codeofli.my.util;
 public class Pair<V1, V2> {
     public V1 val1;
     public V2 val2;
+    public Pair() {}
 
     public Pair(V1 val1, V2 val2) {
         this.val1 = val1;
