45ed12632d0e1e71a96aadc61ccd17c9665f1245
mingzli <nicolaslee5@foxmail.com>
2022-10-08 23:10:06 +0800

    221008

--- src/main/java/codeofli/my/solution_template/find/BinarySearch.java
+++ src/main/java/codeofli/my/solution_template/find/BinarySearch.java
@@ -21,11 +21,11 @@ public class BinarySearch {
      * F,T 右边界
      * @return 返回的index有越界检查，不会越界
      */
-    private int biggerNumberIndexByBS(int[] arr, int x) {
+    private int biggerNumberIndexByBS(int[] arr, int key) {
         int low = 0, high = arr.length - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (arr[mid] >= x) {
+            if (arr[mid] >= key) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
@@ -42,14 +42,57 @@ public class BinarySearch {
      *T, F,左边界
      * @Return 返回的index有越界检查，不会越界
      */
-    private int smallerNumberIndexByBSWithRangeCheck(int[] arr, int x) {
+    private int smallerNumberIndexByBS(int[] arr, int key) {
         int low = 0, high = arr.length - 1;
         int pos = -1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (arr[mid] > x) {
+            if (arr[mid] > key) {
                 high = mid - 1;
-            } else { //此时 <= x
+            } else { //此时 <= key
+                pos = mid;
+                low = mid + 1;
+            }
+        }
+        if (pos == -1) { //有可能比数组中所有数字都小
+            return 0;
+        }
+        return pos;
+    }
+
+    /**
+     * 二分查找<= x的最大值下标，。
+     *T, F,左边界
+     */
+    private int smallerNumberIndexByBS(int[] arr, int key, int fromIndex, int toIndex) {
+        int low = fromIndex, high = toIndex - 1;
+        int pos = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > key) {
+                high = mid - 1;
+            } else { //此时 <= key
+                pos = mid;
+                low = mid + 1;
+            }
+        }
+        if (pos == -1) { //有可能比数组中所有数字都小
+            return 0;
+        }
+        return pos;
+    }
+    /**
+     * 二分查找<= x的最大值下标，。
+     *T, F,左边界
+     */
+    private int smallerNumberIndexByBS(int[] arr, int key, int fromIndex) {
+        int low = fromIndex, high = arr.length - 1;
+        int pos = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > key) {
+                high = mid - 1;
+            } else { //此时 <= key
                 pos = mid;
                 low = mid + 1;
             }
