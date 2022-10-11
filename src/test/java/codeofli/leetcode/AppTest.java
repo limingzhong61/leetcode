9bf84edf91dea755dfce5b1044594d1397969663
mingzli <nicolaslee5@foxmail.com>
2022-10-11 23:49:17 +0800

    2022-10-11

--- src/test/java/codeofli/leetcode/AppTest.java
+++ src/test/java/codeofli/leetcode/AppTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Unit test for simple App.
  */
@@ -16,5 +20,21 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void method() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(1);  //这样调用 add 方法只能存储整形，因为泛型类型的实例为 Integer
+
+        list.getClass().getMethod("add", Object.class).invoke(list, "asd");
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        ArrayList<String> list1 = new ArrayList(); //第一种 情况
+        ArrayList list2 = new ArrayList<String>(); //第二种 情况
     }
 }
