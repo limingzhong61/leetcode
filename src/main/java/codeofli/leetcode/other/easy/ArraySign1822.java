ae72d1eabc0f610eaaa8c8ad7db1d49b0ef6ae3b
mingzli <nicolaslee5@foxmail.com>
2022-10-27 09:42:59 +0800

    22 Oct 27 lc everyday problem

---
+++ src/main/java/codeofli/leetcode/other/easy/ArraySign1822.java
@@ -0,0 +1,20 @@
+package codeofli.leetcode.other.easy;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author: codeofli
+ * @create: 2022-10-27 9:15
+ */
+public class ArraySign1822 {
+    public int arraySign(int[] nums) {
+        int product = 1;
+        for(int x : nums){
+            product *= x;
+        }
+        if(product == 0){
+            return 0;
+        }
+        return product > 0 ? 1 : -1;
+    }
+}
