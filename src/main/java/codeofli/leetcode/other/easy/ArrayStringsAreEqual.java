5191d4eff7e7749938b24211e9bf734cad32d570
mingzli <nicolaslee5@foxmail.com>
2022-11-01 09:34:25 +0800

    22-11-1 lc everyday problem

---
+++ src/main/java/codeofli/leetcode/other/easy/ArrayStringsAreEqual.java
@@ -0,0 +1,19 @@
+package codeofli.leetcode.other.easy;
+
+/**
+ * @author: codeofli
+ * @create: 2022-11-01 9:30
+ */
+public class ArrayStringsAreEqual {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        return join(word1).equals(join(word2));
+    }
+
+    private String join(String[] word1) {
+        StringBuilder sb = new StringBuilder();
+        for(var s : word1){
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+}
