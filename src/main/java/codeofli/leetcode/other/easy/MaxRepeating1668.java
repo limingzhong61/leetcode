df5484dae6336aadf6d2c18daf8a564515c90133
mingzli <nicolaslee5@foxmail.com>
2022-11-03 09:36:34 +0800

    22-11-3 lc every problem

--- src/main/java/codeofli/leetcode/other/easy/MaxRepeating1668.java
+++ src/main/java/codeofli/leetcode/other/easy/MaxRepeating1668.java
@@ -5,4 +5,15 @@ package codeofli.leetcode.other.easy;
  * @create: 2022-11-03 9:23
  */
 public class MaxRepeating1668 {
+    public int maxRepeating(String sequence, String word) {
+        int max = 0;
+        String s = word;
+        for(int i = 1; s.length() <= sequence.length();  i++){
+            if(sequence.contains(word)){
+                max = i;
+            }
+            s = word.repeat(i+1);
+        }
+        return max;
+    }
 }
