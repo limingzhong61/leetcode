b0f956d97cb654fca91289ef61d1af137ecad075
mingzli <nicolaslee5@foxmail.com>
2022-11-10 21:01:22 +0800

    new

--- src/main/java/codeofli/leetcode/contest/c318/ApplyOperations2460.java
+++ src/main/java/codeofli/leetcode/contest/c318/ApplyOperations2460.java
@@ -1,13 +1,35 @@
 package codeofli.leetcode.contest.c318;
 
-import java.util.Arrays;
-
 /**
  * @author: codeofli
  * @create: 2022-11-06 10:31
  */
-public class ApplyOperations {
+public class ApplyOperations2460 {
+    /**
+     * lc: 优化O(1)空间复杂度
+     */
     public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        var res = new int[n];
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i + 1] == nums[i]) {
+                nums[i] = 2 * nums[i];
+                nums[i + 1] = 0;
+            }
+        }
+        int idx = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] != 0){
+                res[idx++] = nums[i];
+            }
+        }
+        for(int i = idx; i < n; i++){
+            res[i] = 0;
+        }
+        return res;
+    }
+
+    public int[] applyOperations2(int[] nums) {
         int n = nums.length;
         var res = new int[n];
         for (int i = 0; i < n - 1; i++) {
