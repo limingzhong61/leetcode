ea09a59ce4a57d5e16352b9ea350647221f7a48f
mingzli <nicolaslee5@foxmail.com>
2022-12-01 09:59:05 +0800

    2022-12-1

--- src/main/java/codeofli/leetcode/other/hard/FreqStack95.java
+++ src/main/java/codeofli/leetcode/other/hard/FreqStack95.java
@@ -7,10 +7,43 @@ import java.util.*;
  * @create: 2022-11-30 9:44
  */
 public class FreqStack95 {
+    /**
+     * lc:双hash
+     */
     class FreqStack {
+        private Map<Integer, Integer> freq;
+        private Map<Integer, Deque<Integer>> group;
+        private int maxFreq;
+
+        public FreqStack() {
+            freq = new HashMap<Integer, Integer>();
+            group = new HashMap<Integer, Deque<Integer>>();
+            maxFreq = 0;
+        }
+
+        public void push(int val) {
+            freq.put(val, freq.getOrDefault(val, 0) + 1);
+            group.putIfAbsent(freq.get(val), new ArrayDeque<Integer>());
+            group.get(freq.get(val)).push(val);
+            maxFreq = Math.max(maxFreq, freq.get(val));
+        }
+
+        public int pop() {
+            int val = group.get(maxFreq).peek();
+            freq.put(val, freq.get(val) - 1);
+            group.get(maxFreq).pop();
+            if (group.get(maxFreq).isEmpty()) {
+                maxFreq--;
+            }
+            return val;
+        }
+    }
+
+
+    class FreqStack1 {
         TreeMap<Integer, Deque<Integer>> freqMap = new TreeMap<>();
         HashMap<Integer,Integer> cntMap = new HashMap<>();
-        public FreqStack() {
+        public FreqStack1() {
 
         }
 
