f437ff7d5d3e7caa16cdcbc3bc09eaa81bd4c54d
mingzli <nicolaslee5@foxmail.com>
2023-01-01 16:55:06 +0800

    2023-1-1

---
+++ src/main/java/lmz/leetcode/other/easy/old/LetterCasePermutation784.java
@@ -0,0 +1,30 @@
+package lmz.leetcode.other.easy.old;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: codeofli
+ * @create: 2022-10-30 9:13
+ */
+public class LetterCasePermutation784 {
+    List<String> res = new ArrayList<>();
+    public List<String> letterCasePermutation(String s) {
+        char[] cs = s.toCharArray();
+        dfs(s,cs,0);
+        return  res;
+    }
+
+    private void dfs(String s, char[] cs,int cur ) {
+        if(cur == s.length()){
+            res.add(String.valueOf(cs));
+            return ;
+        }
+        if(Character.isLetter(cs[cur])){
+            cs[cur] = Character.toUpperCase(cs[cur]);
+            dfs(s,cs,cur + 1);
+            cs[cur] = Character.toLowerCase(cs[cur]);
+            dfs(s,cs,cur + 1);
+        }
+    }
+}
---
+++ src/main/java/lmz/leetcode/other/easy/old/SumOddLengthSubarrays1588.java
@@ -0,0 +1,40 @@
+package lmz.leetcode.other.easy.old;
+
+/**
+ * @author: codeofli
+ * @create: 2022-11-22 10:05
+ */
+public class SumOddLengthSubarrays1588 {
+    /**
+     * 计算idx位置的数组有多少个奇数长度的位置：要为奇数长度，左右两边要么都是奇数，要么都是偶数：
+     */
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum = 0;
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int leftCount = i, rightCount = n - i - 1;
+            int leftOdd = (leftCount + 1) / 2;
+            int rightOdd = (rightCount + 1) / 2;
+            int leftEven = leftCount / 2 + 1;
+            int rightEven = rightCount / 2 + 1;
+            sum += arr[i] * (leftOdd * rightOdd + leftEven * rightEven);
+        }
+        return sum;
+    }
+
+    public int sumOddLengthSubarrays1(int[] arr) {
+        int n = arr.length,res = 0;
+        for(int len = 1; len <= n; len +=2) {
+            int temp = 0;
+            for(int i = 0; i < len; i++){
+                temp += arr[i];
+            }
+            res += temp;
+            for(int i = len; i < n; i++){
+                temp = temp - arr[i - len] + arr[i];
+                res += temp;
+            }
+        }
+        return res;
+    }
+}
