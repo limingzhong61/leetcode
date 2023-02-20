20eac0a061c6940b3d440e65a65e2e5e3f8a9b3b
mzli <1162314270@qq.com>
2023-02-20 13:34:45 +0800

    2023-2-20

---
+++ src/main/java/lmz/leetcode/contest/c333/MinOperations.java
@@ -0,0 +1,29 @@
+package lmz.leetcode.contest.c333;
+
+public class MinOperations {
+    /**
+     *
+     */
+    public int minOperations(int n) {
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return  1;
+        }
+        int x = 2,lastX = 2;
+        while (x< n) {
+            lastX = x;
+            x *= 2;
+        }
+        int left = n - lastX, right = x - n;
+
+        return  Math.min(1+ minOperations(left),1 +minOperations(right));
+    }
+
+    public static void main(String[] args) {
+        MinOperations minOperations = new MinOperations();
+        System.out.println(minOperations.minOperations(39));
+        System.out.println(minOperations.minOperations(54));
+    }
+}
