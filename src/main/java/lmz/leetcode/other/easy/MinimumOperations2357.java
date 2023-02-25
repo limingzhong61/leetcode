609627b7059a1ce09cddad9510fa40a526696ea5
mingzli <nicolaslee5@foxmail.com>
2023-02-25 11:18:10 +0800

    2023-2-25

---
+++ src/main/java/lmz/leetcode/other/easy/MinimumOperations2357.java
@@ -0,0 +1,41 @@
+package lmz.leetcode.other.easy;
+
+/**
+ * @author: limingzhong
+ * @create: 2023-02-24 8:20
+ */
+public class MinimumOperations2357 {
+    public int minimumOperations(int[] nums) {
+        int n = nums.length;
+        int cnt = 0;
+        while(true){
+
+            int min = Integer.MAX_VALUE;
+            for(int i = 0; i < n; i++){
+                if(nums[i] != 0){
+                    min = Math.min(min,nums[i]);
+                }
+            }
+
+            if(Integer.MAX_VALUE == min){
+                break;
+            }
+            cnt++;
+            boolean notZero = false;
+            for(int i = 0; i < n; i++){
+                if(nums[i] != 0){
+                    nums[i] -= min;
+                }
+
+            }
+        }
+        return cnt;
+    }
+}
+
+
+
+
+
+
+
