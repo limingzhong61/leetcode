0d9aedb0a91dab5352a46036669a5f3241fdfbfd
mingzli <nicolaslee5@foxmail.com>
2023-03-03 11:15:45 +0800

    2023-3-3

---
+++ src/main/java/lmz/leetcode/other/medium/GetFolderNames1487.java
@@ -0,0 +1,68 @@
+package lmz.leetcode.other.medium;
+
+import lmz.my.leetcode.TransformUtil;
+
+import java.util.*;
+
+/**
+ * @author: limingzhong
+ * @create: 2023-03-03 8:05
+ */
+public class GetFolderNames1487 {
+    public String[] getFolderNames(String[] names) {
+        Map<String, Integer> map = new HashMap<>();
+        String[] res = new String[names.length];
+        int idx = 0;
+        for (String s : names) {
+            if (!map.containsKey(s)) {
+                map.put(s, 0);
+                res[idx++] = s;
+            } else {
+                int nextNum = map.get(s) + 1;
+                String next = s + '(' + nextNum + ')';
+                while (map.containsKey(next)) {
+                    next = s + '(' + nextNum + ')';
+                    nextNum++;
+                }
+                res[idx++] = next;
+                map.put(s, nextNum);
+                map.put(next,0);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        GetFolderNames1487 getFolderNames1487 = new GetFolderNames1487();
+        System.out.println(Arrays.toString(getFolderNames1487.getFolderNames(
+                TransformUtil.toStringArray("[\"kaido\",\"kaido(1)\",\"kaido\",\"kaido(1)\",\"kaido(2)\"]"))));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
