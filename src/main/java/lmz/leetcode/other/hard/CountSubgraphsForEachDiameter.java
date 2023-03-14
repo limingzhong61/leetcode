c529d1109ca86103bdd9bbfe5463638d64853639
mingzli <nicolaslee5@foxmail.com>
2023-03-14 10:03:40 +0800

    2023-3-14

--- src/main/java/lmz/leetcode/other/hard/CountSubgraphsForEachDiameter.java
+++ src/main/java/lmz/leetcode/other/hard/CountSubgraphsForEachDiameter.java
@@ -5,7 +5,7 @@ package lmz.leetcode.other.hard;
  * @create: 2023-03-12 21:32
  */
 public class CountSubgraphsForEachDiameter {
-    public int[] countSubgraphsForEachDiameter(int n, int[][] edges) {
-
-    }
+    //public int[] countSubgraphsForEachDiameter(int n, int[][] edges) {
+    //
+    //}
 }
