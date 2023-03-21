1058d0c841be579e4c251ccdef22dce2e892301e
mzli <1162314270@qq.com>
2023-03-21 09:59:21 +0800

    x

--- src/main/java/lmz/leetcode/find/kth/KSmallestPairsII061.java
+++ src/main/java/lmz/leetcode/find/kth/KSmallestPairsII061.java
@@ -1,5 +1,6 @@
 package lmz.leetcode.find.kth;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -9,16 +10,32 @@ import java.util.PriorityQueue;
  */
 public class KSmallestPairsII061 {
     /**
+     * 优先队列
      * 加减溢出
      * -109 <= nums1[i], nums2[i] <= 109
      */
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (int) ((long) b[0] + b[1] - a[0] - a[1]));
-        int x = nums1[0];
-        int y = nums2[0];
-        for (int i = 0; i < nums2.length; i++) {
-            if(pq)
-            pq.add()
+        PriorityQueue<int[]> pq = new PriorityQueue<>(k, (o1, o2)->{
+            return nums1[o1[0]] + nums2[o1[1]] - nums1[o2[0]] - nums2[o2[1]];
+        });
+        List<List<Integer>> ans = new ArrayList<>();
+        int m = nums1.length;
+        int n = nums2.length;
+        for (int i = 0; i < Math.min(m, k); i++) {
+            pq.offer(new int[]{i,0});
         }
+        while (k-- > 0 && !pq.isEmpty()) {
+            int[] idxPair = pq.poll();
+            List<Integer> list = new ArrayList<>();
+            list.add(nums1[idxPair[0]]);
+            list.add(nums2[idxPair[1]]);
+            ans.add(list);
+            if (idxPair[1] + 1 < n) {
+                pq.offer(new int[]{idxPair[0], idxPair[1] + 1});
+            }
+        }
+
+        return ans;
     }
+
 }
