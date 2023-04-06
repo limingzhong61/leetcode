fef72cbd7d9924d743928fc55db9977b35008580
mzli <1162314270@qq.com>
2023-04-06 22:39:17 +0800

    2023-4-6

--- src/main/java/lmz/my/Test.java
+++ src/main/java/lmz/my/Test.java
@@ -4,5 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
         //System.out.println(10^5);
+        byte a = (byte) 128;
+        Byte b = (byte) 128;
+        System.out.println(a);
     }
 }
