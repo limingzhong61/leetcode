18f6b399b014736d502e1fd7bff1cfc288c814da
mzli <1162314270@qq.com>
2023-04-08 21:10:45 +0800

    2023-4-8

--- src/main/java/lmz/leetcode/other/medium/maskPII831.java
+++ src/main/java/lmz/leetcode/other/medium/maskPII831.java
@@ -1,17 +1,15 @@
 package lmz.leetcode.other.medium;
 
-import lmz.newcoder.Main;
-
 /**
  * @author: limingzhong
- * @create: 2023-04-01 13:49
+ * @create: 2023-04-01 9:31
  */
-public class maskPII831 {
+public class MaskPII831 {
     public String maskPII(String s) {
         int n = s.length();
         char[] cs = s.toCharArray();
         StringBuilder sb = new StringBuilder();
-        if (!Character.isLetter(cs[0])) {
+        if (Character.isDigit(cs[0])) {
             int cnt = 0;
             for (int i = 0; i < n; i++) {
                 if (Character.isDigit(cs[i])) {
@@ -19,37 +17,33 @@ public class maskPII831 {
                 }
             }
             cnt -= 10;
-            int i = 0;
+            int i  = 0;
             if (cnt > 0) {
                 sb.append('+');
-                while(cnt > 0){
+
+                for(; i < n && cnt > 0; i++){
                     if (Character.isDigit(cs[i])) {
-                        sb.append('*');
+                        sb.append(cs[i]);
                         cnt--;
                     }
-                    i++;
                 }
-                sb.append('-');
             }
-
-            int dCnt = 0;
+            int x = 0;
             for (; i < n; i++) {
                 if (Character.isDigit(cs[i])) {
-
-                    if(dCnt >= 6){
+                    if(cnt > 0){
                         sb.append(cs[i]);
-                    }else{
-                        sb.append('*');
-                    }
-                    dCnt++;
-                    if(dCnt % 3 == 0 && dCnt <= 6){
+                    }else if (cnt == 0) {
                         sb.append('-');
+                    }else{
+
                     }
                     cnt--;
                 }
             }
         } else { // email
             sb.append(Character.toLowerCase(cs[0]));
+            System.out.println(Character.toLowerCase(cs[0]));
             for (int i = 0; i < 5; i++)
                 sb.append('*');
             int idx = s.indexOf('@');
@@ -64,10 +58,5 @@ public class maskPII831 {
             }
         }
         return sb.toString();
-    }
-
-    public static void main(String[] args) {
-        maskPII831 maskPII831 = new maskPII831();
-        System.out.println(maskPII831.maskPII("1(234)567-890"));
     }
 }
--- src/main/java/lmz/my/Test.java
+++ src/main/java/lmz/my/Test.java
@@ -6,6 +6,7 @@ public class Test {
         //System.out.println(10^5);
         byte a = (byte) 128;
         Byte b = (byte) 128;
+        Integer integer = Integer.valueOf(1);
         System.out.println(a);
     }
 }
