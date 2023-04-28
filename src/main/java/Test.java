5e32d2b9cceb0a0eb5e644781f251af9915217ca
mzli <1162314270@qq.com>
2023-04-28 14:54:21 +0800

    2023-4-28

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,26 +1,23 @@
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * @author: limingzhong
  * @create: 2023-04-22 13:39
  */
 public class Test {
-    public int longestArithSeqLength(int[] nums) {
-        int n = nums.length;
-        //i,j,以i结尾公差为的j的状态。
-        int[][] f = new int[n][1000+1];
-        int max = 2;
-        Arrays.fill(f[0],1);
-        for(int i = 1; i < n; i++){
-            // f[i] = 2; // 至少为2
-            Arrays.fill(f[i],1);
-            for(int j = 0; j < i; j++){
-                int diff = nums[j] - nums[i];
-                diff += 500;
-                f[i][diff] = f[j][diff] + 1;
-                max = Math.max(f[i][diff],max);
-            }
+    public void longestArithSeqLength(int[] nums) {
+        Scanner cin = new Scanner(System.in);
+        int a, b;
+        while (cin.hasNextInt()) {
+            a = cin.nextInt();
+            b = cin.nextInt();
+
+            System.out.println(a);
         }
-        return max;
+        //cin.close();
     }
 }
