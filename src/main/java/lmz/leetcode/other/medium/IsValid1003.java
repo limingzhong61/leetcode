c631ef5aef19071c4c9db8ed8be777544659b88b
mzli <1162314270@qq.com>
2023-05-03 11:35:43 +0800

    2023-5-3

---
+++ src/main/java/lmz/leetcode/other/medium/IsValid1003.java
@@ -0,0 +1,19 @@
+package lmz.leetcode.other.medium;
+
+import java.util.Arrays;
+
+/**
+ * @author: limingzhong
+ * @create: 2023-05-03 10:55
+ */
+public class IsValid1003 {
+    class Solution {
+        /**
+         * 双指针，因为s如果成立，则a、b、c的数量相等
+         */
+        public boolean isValid(String s) {
+            return true;
+        }
+
+    }
+}
