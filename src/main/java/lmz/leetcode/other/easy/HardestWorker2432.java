fe443492581de11374c2acfbfb16aef8ff31d352
mzli <1162314270@qq.com>
2023-05-05 15:24:10 +0800

    2023-5-5

--- src/main/java/lmz/leetcode/other/easy/HardestWorker2432.java
+++ src/main/java/lmz/leetcode/other/easy/HardestWorker2432.java
@@ -29,4 +29,26 @@ public class HardestWorker2432 {
             return resId;
         }
     }
+    class Solution1 {
+        public int hardestWorker(int n, int[][] logs) {
+            int startTime = 0,resId = n+1,maxLen = 0;
+            for(int i = 0; i < logs.length; i++){
+                int len = logs[i][1] - startTime;
+                startTime = logs[i][1];
+                if(len > maxLen){
+                    maxLen = len;
+                }
+            }
+
+            startTime = 0;
+            for(int i = 0; i < logs.length; i++){
+                int len = logs[i][1] - startTime;
+                startTime = logs[i][1];
+                if(len == maxLen){
+                    resId = Math.min(resId,logs[i][0]);
+                }
+            }
+            return resId;
+        }
+    }
 }
