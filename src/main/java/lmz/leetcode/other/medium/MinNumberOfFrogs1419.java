21a014267368f936922a7a2960a44b564e4c79a8
mzli <1162314270@qq.com>
2023-05-07 20:39:36 +0800

    2023-5-7

---
+++ src/main/java/lmz/leetcode/other/medium/MinNumberOfFrogs1419.java
@@ -0,0 +1,17 @@
+package lmz.leetcode.other.medium;
+
+/**
+ * @author: limingzhong
+ * @create: 2023-05-06 13:35
+ */
+public class MinNumberOfFrogs1419 {
+    class Solution {
+        /**
+         * 模拟题
+         */
+        public int minNumberOfFrogs(String croakOfFrogs) {
+            return 0;
+        }
+
+    }
+}
