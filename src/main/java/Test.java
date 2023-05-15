b075f87131d0bc6da0f4ae6a24690a6a75e0f881
mzli <1162314270@qq.com>
2023-05-15 18:02:55 +0800

    2023-5-15

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,7 @@ import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -10,12 +11,7 @@ import java.util.Scanner;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Math.round(11.5));
-        System.out.println(Math.round(-11.5));
-        String s = "xx";
-        s.toUpperCase();
-        System.out.println(s);
-        Double x = 1.2;
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
     }
 
     Test() {
