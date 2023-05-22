526149379462368e76d6fa6bbb9d45ddac0f0d0c
mingzli <nicolaslee5@foxmail.com>
2023-05-22 17:53:33 +0800

    2023-5-22

--- src/main/java/lmz/my/Test.java
+++ src/main/java/lmz/my/Test.java
@@ -1,12 +1,16 @@
 package lmz.my;
 
+import java.util.HashMap;
+
 public class Test {
 
     public static void main(String[] args) {
+        //Integer i = null;
+        //System.out.println(i == 0);
+        HashMap<Integer,Integer> a = new HashMap<>(10);
+        a.put(1,0);
+        a.put(1,1);
+        System.out.println(a.get(1));
         //System.out.println(10^5);
-        byte a = (byte) 128;
-        Byte b = (byte) 128;
-        Integer integer = Integer.valueOf(1);
-        System.out.println(a);
     }
 }
