36b4ed4caa6728cc0498ff4bc93ebf9a3b2f845b
mzli <1162314270@qq.com>
2023-05-28 15:56:04 +0800

    2023-5-28

--- src/main/java/lmz/algorithm/two_pointer/FindAnagrams438.java
+++ src/main/java/lmz/algorithm/two_pointer/FindAnagrams438.java
@@ -5,12 +5,38 @@ import java.util.List;
 
 public class FindAnagrams438 {
     /**
+     * 23.5.28
+     * s 和 p 仅包含小写字母
+     */
+    public List<Integer> findAnagrams(String s, String p) {
+        if(s.length() < p.length()){
+            return null;
+        }
+        int letterCnt = 26;
+        int[] cntP = new int[letterCnt];
+        for(char c : p.toCharArray()){
+            cntP[c - 'p']++;
+        }
+        int[] cntS = new int[letterCnt];
+        int lenP = p.length();
+        char[] cs = s.toCharArray();
+        for(int i = 0; i < lenP; i++){
+            cntS[cs[i] -'a']++;
+        }
+        
+
+
+
+
+    }
+
+    /**
      * 双指针+hash
      * 异位词：只要字符出现次数相同即可，hash统计
      * s 和 p 仅包含小写字母
      * 1 <= s.length, p.length <= 3 * 104
      */
-    public List<Integer> findAnagrams(String s, String p) {
+    public List<Integer> findAnagrams2(String s, String p) {
         int pLen = p.length(), sLen = s.length();
         if (sLen < pLen) {
             return new ArrayList<>();
