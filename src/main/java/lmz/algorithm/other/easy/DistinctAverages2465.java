a0b85ee3db1e0cb0995f85c976e3a0f513e930bb
mzli <1162314270@qq.com>
2023-06-04 10:43:03 +0800

    2023-6-4

---
+++ src/main/java/lmz/algorithm/other/easy/DistinctAverages2465.java
@@ -0,0 +1,19 @@
+package lmz.algorithm.other.easy;
+
+import java.util.Arrays;
+
+/**
+ * @author: limingzhong
+ * @create: 2023-06-04 10:40
+ */
+public class DistinctAverages2465 {
+    /**
+     * 给你一个下标从 0 开始长度为 偶数 的整数数组 nums 。
+     */
+    public int distinctAverages(int[] nums) {
+        Arrays.sort(nums);
+        for(int left = 0,right = nums.length -1; left < right; left++,right--){
+            
+        }
+    }
+}
