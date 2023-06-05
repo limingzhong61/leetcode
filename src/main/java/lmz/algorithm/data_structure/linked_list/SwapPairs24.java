953f15a4b4a3efd383efdc846a79564940537a4b
mzli <1162314270@qq.com>
2023-06-05 19:06:54 +0800

    2023-6-5

--- src/main/java/lmz/algorithm/data_structure/linked_list/SwapPairs24.java
+++ src/main/java/lmz/algorithm/data_structure/linked_list/SwapPairs24.java
@@ -3,7 +3,31 @@ package lmz.algorithm.data_structure.linked_list;
 import lmz.algorithm.data_structure.linked_list.util.ListNode;
 
 public class SwapPairs24 {
+
+
     public ListNode swapPairs(ListNode head) {
+        if(head == null ||head.next == null){
+            return  head;
+        }
+        ListNode dummy = new ListNode(0,head);
+        ListNode p = dummy,pre = dummy,cur = dummy.next;
+        while (cur != null && cur.next != null){
+            pre = cur;
+            cur = cur.next;
+            ListNode next= cur.next;
+
+            p.next = cur;
+            cur.next = pre;
+            pre.next = next;    // 奇数情况
+            p = pre;
+            cur = next;
+        }
+        return  dummy.next;
+    }
+
+
+
+    public ListNode swapPairs1(ListNode head) {
     //    0 <= Node.val <= 100
         if(head == null || head.next == null){
             return head;
