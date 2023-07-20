77ae038e8aea92c0a612f0b6caf87da5fe3d9525
mingzli <nicolaslee5@foxmail.com>
2023-07-20 14:43:57 +0800

    2023-7-20

---
+++ src/main/java/test/ReverseLinkedList.java
@@ -0,0 +1,47 @@
+package test;
+
+
+
+/**
+ * @author: limingzhong
+ * @create: 2023-07-14 14:48
+ */
+class  ListNode{
+    int value;
+    ListNode next;
+    public  ListNode(int value,ListNode next){
+        this.value = value;
+        this.next = next;
+    }
+}
+public class ReverseLinkedList {
+    ListNode reverse(ListNode root){
+        ListNode pre = null,cur = root;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode dummy = new ListNode(-1,null);
+        ListNode cur = dummy;
+        int n = 5;
+        for(int i = 0; i < n; i++){
+            cur.next = new ListNode(i,null);
+            cur = cur.next;
+        }
+        ReverseLinkedList reverseLinkedList = new ReverseLinkedList();
+        ListNode root = reverseLinkedList.reverse(dummy.next);
+
+        cur = root;
+        while(cur != null){
+            System.out.println(cur.value);
+            cur = cur.next;
+        }
+
+    }
+}
