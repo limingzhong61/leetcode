43162ff1acd241afb530fc555cea11a428cc91e5
mingzli <nicolaslee5@foxmail.com>
2023-09-01 08:52:29 +0800

    2023-9-1

---
+++ src/main/java/com/lmz/algorithm/p/p_2000/WaysToBuyPensPencils2240.java
@@ -0,0 +1,13 @@
+package com.lmz.algorithm.p.p_2000;
+
+/**
+ * @author: limingzhong
+ * @create: 2023-09-01 8:51
+ */
+public class WaysToBuyPensPencils2240 {
+    public long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        for(int i = 0; i * cost1 <= total; i++){
+            
+        }
+    }
+}
