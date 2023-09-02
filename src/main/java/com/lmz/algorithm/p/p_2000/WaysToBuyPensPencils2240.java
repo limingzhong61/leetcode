a8dc94b6ee0204b29fd736b37b927960b09a4e13
mingzli <nicolaslee5@foxmail.com>
2023-09-02 21:25:15 +0800

    2023-9-2

--- src/main/java/com/lmz/algorithm/p/p_2000/WaysToBuyPensPencils2240.java
+++ src/main/java/com/lmz/algorithm/p/p_2000/WaysToBuyPensPencils2240.java
@@ -6,8 +6,16 @@ package com.lmz.algorithm.p.p_2000;
  */
 public class WaysToBuyPensPencils2240 {
     public long waysToBuyPensPencils(int total, int cost1, int cost2) {
-        for(int i = 0; i * cost1 <= total; i++){
-            
+        long ans = 0;
+        for (int i = 0; i * cost1 <= total; i++) {
+            int remain = total - i * cost1;
+            int d = remain / cost2;
+            // 能整除？
+            if (remain % cost2 == 0) {
+                d++;
+            }
+            ans = ans + d;
         }
+        return ans;
     }
 }
